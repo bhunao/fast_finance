66e6e3db7fd877cd8ce5a4eee494a37d7a859222
bhunao <bhunao@gmail.com>
2024-08-28 19:48:27 -0300

    base for db

--- web/app/core/models.py
+++ web/app/core/models.py
@@ -1,15 +1,18 @@
 from typing import Annotated
 
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel
+from sqlmodel import Field
 
 
 class UserFields:
-    id = Annotated[int, Field()]
+    id = Annotated[int, Field(primary_key=True)]
     username = Annotated[str, Field(title="Usuário")]
     password = Annotated[str, Field(title="Senha")]
 
 
-class UserBase(SQLModel):
+class UserBase(SQLModel, table=True):
+    __tablename__ = "user"
+
     id: UserFields.id
     username: UserFields.username
     password: UserFields.password
