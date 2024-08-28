ee9e6be1911df94ccc10192b5ff823503f37f8c2
bhunao <bhunao@gmail.com>
2024-08-28 20:04:14 -0300

    add sqlmodel to script alembic script template

--- web/app/alembic/versions/1e601c0bec61_first_migration.py
+++ web/app/alembic/versions/1e601c0bec61_first_migration.py
@@ -1,21 +1,23 @@
 """first migration
 
-Revision ID: c70771483e96
+Revision ID: 1e601c0bec61
 Revises: 
-Create Date: 2024-08-28 22:47:12.382399
+Create Date: 2024-08-28 23:02:38.667723
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
 from alembic import op
+
 import sqlalchemy as sa
+import sqlmodel.sql.sqltypes
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'c70771483e96'
-down_revision: Union[str, None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = '1e601c0bec61'
+down_revision: str | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
