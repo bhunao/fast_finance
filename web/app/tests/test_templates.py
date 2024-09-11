b5157cc87a593d34513dbfd3ce497fa8a8f81b73
bhunao <bhunao@gmail.com>
2024-09-11 20:26:07 -0300

    helper function on template tests

--- web/app/tests/test_templates.py
+++ web/app/tests/test_templates.py
@@ -10,14 +10,19 @@ class TemplateResponse(Response):
     template: Template = Template("")
 
 
+def has_template(response: TemplateResponse):
+    assert hasattr(response, "template")
+    assert isinstance(response.template, Template)
+    return True
+
+
 def test_transaction_all(client: TestClient):
     response: TemplateResponse = client.get(  # pyright: ignore[reportAssignmentType]
         "/web/transactions/all",
     )
-
     assert response.status_code == 200
-    assert response.template
-    assert isinstance(response.template, Template)
+    assert has_template(response)
+    assert response.template.name == "table.html"
 
 
 @pytest.mark.skip("TDD, have to create the endpoint.")
@@ -26,5 +31,5 @@ def test_transaction_dashboard(client: TestClient):
         "/web/transactions/dashboard",
     )
     assert response.status_code == 200
-    assert response.template
-    assert isinstance(response.template, Template)
+    assert has_template(response)
+    assert response.template.name == "dashboard.html"
